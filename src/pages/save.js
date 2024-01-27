12b6a6a0fa6c7dc06e1411be5836b0c9157382a6
최성훈 <chltjdgns1009@gmail.com>
2024-01-28 05:24:37 +0900

    정리

--- src/pages/save.js
+++ src/pages/save.js
@@ -1,11 +1,124 @@
 import React from 'react';
+import styled from 'styled-components';
+import option from "../assets/imgs/option.svg"
+import friends from "../assets/imgs/friends.png"
+import Button from '../components/common/Button';
 
 const Save = () => {
   return (
-    <div>
-      보관함
-    </div>
+    <Container>
+      <Head>
+        <Title>보관함</Title>
+        <Option src={option} />
+      </Head>
+      <ContentContainer>
+        <Friends src={friends} />
+        <Foot>
+          <TextContainer>
+            <Place>인생네컷 숙대입구역점</Place>
+            <Date>2022.09.30</Date>
+          </TextContainer>
+          <Buttons>
+            <Button text="사진 저장" backgroundColor="#5182F6" color="#FFFFFF" width="168.5px" />
+            <Button text="프로필에 업로드" backgroundColor="#959DB229" color="#24272E99" width="168.5px" />
+          </Buttons>
+        </Foot>
+      </ContentContainer>
+    </Container>
   );
 };
+
+const Title = styled.p`
+  color: ${({ theme }) => theme.content.tertiary};
+  width: 321px;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 32px;
+  letter-spacing: -0.24px;
+`
+
+const Container = styled.div`
+  margin: 15px;
+  width: 92vw;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+`;
+
+const Head = styled.div`
+  display: flex;
+  padding: 0px 4px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`
+
+const Option = styled.img`
+  width: 32px;
+  height: 32px;
+`
+
+const ContentContainer = styled.div`
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  border-radius: 16px;
+  border: 1px solid ${({ theme }) => theme.border.primary};
+  background: ${({ theme }) => theme.group.primary};
+`
+
+const Friends = styled.img`
+  border-radius: 16px 16px 0 0;
+`
+
+const Foot = styled.div`
+  display: flex;
+  padding: 12px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 12px;
+  height: auto;
+  width: 100%;
+`
+
+const TextContainer = styled.div`
+  display: flex;
+  padding: 4px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+  height: auto;
+  width: 100%;
+`
+
+const Place = styled.p`
+  color: ${({ theme }) => theme.content.primary};
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: -0.2px;
+`
+
+const Date = styled.p`
+  color: ${({ theme }) => theme.content.secondary};
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.14px;
+`
+
+const Buttons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  height: auto;
+`
 
 export default Save;
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,11 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import search from "../assets/imgs/offSearch.svg"
+import cut1 from "../assets/imgs/cut1.png"
+import cut2 from "../assets/imgs/cut2.png"
+import cut3 from "../assets/imgs/cut3.png"
+import cut4 from "../assets/imgs/cut4.png"
+import placeImg from "../assets/imgs/place.svg"
 
 const Search = () => {
   return (
-    <div>
-      검색
-    </div>
+    <FlexContainer>
+      <SearchInput>
+        <InputContainer>
+          <SearchImg src={search} />
+          <Input placeholder='검색' />
+        </InputContainer>
+      </SearchInput>
+      <Photoes>
+        <Frame>
+          <Img src={cut1} />
+          <PlaceContainer>
+            <PlaceImg src={placeImg} />
+            <PlaceText>포토이즘 하이브사옥점</PlaceText>
+          </PlaceContainer>
+        </Frame>
+        <Frame>
+          <Img src={cut2} />
+          <PlaceContainer>
+            <PlaceImg src={placeImg} />
+            <PlaceText>포토이즘 하이브사옥점</PlaceText>
+          </PlaceContainer>
+        </Frame>
+        <Frame>
+          <Img src={cut3} />
+          <PlaceContainer>
+            <PlaceImg src={placeImg} />
+            <PlaceText>포토이즘 하이브사옥점</PlaceText>
+          </PlaceContainer>
+        </Frame>
+        <Frame>
+          <Img src={cut4  } />
+          <PlaceContainer>
+            <PlaceImg src={placeImg} />
+            <PlaceText>포토이즘 하이브사옥점</PlaceText>
+          </PlaceContainer>
+        </Frame>
+      </Photoes>
+    </FlexContainer>
   );
 };
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const SearchInput = styled.div`
+  display: flex;
+  padding: 12px;
+  align-items: flex-start;
+  gap: 8px;
+`
+
+const InputContainer = styled.div`
+  display: flex;
+  padding: 12px 16px;
+  align-items: flex-start;
+  gap: 10px;
+  border-radius: 12px;
+  border: 1px solid ${({ theme }) => theme.border.secondary};
+  width: 100%;
+`
+
+const SearchImg = styled.img`
+  width: 24px;
+  height: 24px;
+`
+
+const Input = styled.input`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: -0.16px;
+  color: ${({ theme }) => theme.content.teritary};
+  width: 100%;
+  border: none;
+`
+
+const Photoes = styled.div`
+  display: flex;
+  padding: 12px;
+  justify-content: center;
+  align-items: flex-start;
+  align-content: flex-start;
+  gap: 12px;
+  flex-wrap: wrap;
+`
+
+const Frame = styled.div`
+  width: 178.5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Img = styled.img`
+  height: auto;
+  border-radius: 6px;
+`
+
+const PlaceContainer = styled.div`
+  display: flex;
+  padding: 8px 4px;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+`
+
+const PlaceImg = styled.img`
+  width: 16px;
+  height: 16px;
+`
+
+const PlaceText = styled.p`
+
+`
+
 
 export default Search;
