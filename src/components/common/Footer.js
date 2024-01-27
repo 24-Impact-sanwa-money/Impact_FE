961cd091ce7a8eb9b3960adeeae17239ff3d1751
최성훈 <chltjdgns1009@gmail.com>
2024-01-27 22:48:25 +0900

    footer route

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,28 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-import peed from "../../assets/imgs/peed.svg";
-import save from "../../assets/imgs/save.svg";
-import search from "../../assets/imgs/search.svg";
-import profile from "../../assets/imgs/profile.svg"
-
+import onPeed from "../../assets/imgs/onPeed.svg";
+import offPeed from "../../assets/imgs/offPeed.svg";
+import onSave from "../../assets/imgs/onSave.svg";
+import offSave from "../../assets/imgs/offSave.svg";
+import onSearch from "../../assets/imgs/onSearch.svg";
+import offSearch from "../../assets/imgs/offSearch.svg";
+import onProfile from "../../assets/imgs/onProfile.svg";
+import offProfile from "../../assets/imgs/offProfile.svg";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const loc = useLocation();
+  const nav = useNavigate();
+
   return (
     <Footers>
-      <Button>
-        <Img src={peed} />
+      <Button selected={loc.pathname === "/"} onClick={() => nav("/")}>
+        <Img selected={loc.pathname === "/"} onImg={onPeed} offImg={offPeed} />
         피드
       </Button>
-      <Button>
-        <Img src={save} />
+      <Button selected={loc.pathname === "/save"} onClick={() => nav("/save")}>
+        <Img selected={loc.pathname === "/save"} onImg={onSave} offImg={offSave} />
         보관함
       </Button>
-      <Button>
-        <Img src={search} />
+      <Button selected={loc.pathname === "/search"} onClick={() => nav("/search")}>
+        <Img selected={loc.pathname === "/search"} onImg={onSearch} offImg={offSearch} />
         검색
       </Button>
-      <Button>
-        <Img src={profile} />
+      <Button selected={loc.pathname === "/profile"} onClick={() => nav("/profile")}>
+        <Img selected={loc.pathname === "/profile"} onImg={onProfile} offImg={offProfile} />
         프로필
       </Button>
     </Footers>
@@ -49,12 +56,13 @@ const Button = styled.button`
   justify-content: center;
   align-items: center;
   gap: 5px;
-  color: ${({ theme }) => theme.content.primary};
+  color: ${({ selected,theme }) => (selected ? theme.content.primary : theme.content.tertiary)};
 `;
 
 const Img = styled.img`
   width: 28px;
   height: 28px;
-`
+  content: url(${({ selected, onImg, offImg }) => (selected ? onImg : offImg)});
+`;
 
 export default Footer;
---
+++ src/pages/save.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Save = () => {
+  return (
+    <div>
+      보관함
+    </div>
+  );
+};
+
+export default Save;
